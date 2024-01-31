2df0e88c09b34742fdc972261c5f6cf530317699
dariusgatsby <145069350+dariusgatsby@users.noreply.github.com>
2024-01-31 01:44:13 -0800

    added smtp, random letter picker

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import pandas as pd
 import random, os
 import smtplib
 import datetime as dt
-import letter_templates
+
+gmail_pass = "x"
+my_email = "x"
 
 # 1. Update the birthdays.csv
 birthday_list = pd.read_csv("birthdays.csv")
@@ -13,11 +15,18 @@ today = day.day
 month = day.month
 print(today, month)
 
-# 2. Check if today matches a birthday in the birthdays.csv
+
 for i in range(len(birthday_list)):
     if birthday_list.iloc[i]['month'] == month and birthday_list.iloc[i]['day'] == today:
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual
-# name from birthdays.csv
+
         letter = random.choice(os.listdir('letter_templates'))
-        with open()
-# 4. Send the letter generated in step 3 to that person's email address.
+        with open(f"letter_templates/{letter}", 'r') as bday_file:
+            birthday_letter = bday_file.read()
+            # (birthday_letter.replace("[NAME]", birthday_list.iloc[i]['name']))
+            bday_message = birthday_letter.replace("[NAME]", birthday_list.iloc[i]['name'])
+        with smtplib.SMTP('smtp.gmail.com', port=587) as connection:
+            connection.starttls()
+            connection.login(my_email, gmail_pass)
+            connection.sendmail(from_addr=my_email, to_addrs="dariusid08@gmail.com", msg=f"Subject:Happy Birthday!!\n\n"
+                                                                                         f"{bday_message}")
+
